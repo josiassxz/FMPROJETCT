20557f4c7b74915ebc3f8bb06ea9b37717249435
josiassxz <josiassxz@hotmail.com>
2023-06-17 13:38:21 -0300

    ajuste entidade equipamentos

--- src/main/java/com/project/fmproject/domain/model/Equipamentos.java
+++ src/main/java/com/project/fmproject/domain/model/Equipamentos.java
@@ -8,45 +8,6 @@ import java.time.LocalDate;
 import java.util.ArrayList;
 import java.util.List;
 
-//@Entity
-//@Data
-//public class Equipamentos {
-//
-//    @Id
-//    @GeneratedValue(strategy = GenerationType.IDENTITY)
-//    private Long id;
-//
-//    private String tag;
-//
-//    private String descricao;
-//
-//    private String localizacao;
-//
-//    private String categoria;
-//
-//    private String condicao;
-//
-//    private String categoriaEquipamento;
-//
-//    private String inspecaoExterna;
-//
-//    private String inspecaoInterna;
-//
-//    private String proximaInspecao;
-//
-//    private String tipoInspecao;
-//
-//
-//    @ManyToOne
-//    @JsonIgnoreProperties("empresa")
-//    @JoinColumn(name = "empresa_id")
-//    private Empresa empresa;
-//
-//    @JsonIgnoreProperties("equipamento")
-//    @OneToMany(mappedBy = "equipamento", cascade = CascadeType.ALL, orphanRemoval = true)
-//    private List<Documentos> documentos = new ArrayList<>();
-//}
-
 
 @Entity
 @Data
